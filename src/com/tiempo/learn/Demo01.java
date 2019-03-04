ec7d9b396813326c92a2c97b3190756b56fb2962
Vicente Monsalve (Programador) <vicente.monsalve@outlook.com>
2019-03-03 22:52:35 -0300

    Tipos de variables

---
+++ src/com/tiempo/learn/Demo01.java
@@ -0,0 +1,27 @@
+package com.tiempo.learn;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		
+		String testMessage = "Esto es otro mensaje";
+		String mySecondMessage = " demo";
+		
+		System.out.println("Hola Mundo!!");
+		System.out.println(testMessage+mySecondMessage);
+		
+		int firstNumber = 55;
+		int secondNumber = 5;
+		
+		int result = (firstNumber + secondNumber) * 5;
+		
+		System.out.println(result);
+		
+		double myValue = 1235.67894;
+		double mySecondValue = 3.5594588587475;
+		System.out.println(myValue + mySecondValue);
+		
+		
+	}
+
+}
---
+++ src/com/tiempo/learn/Demo02.java
@@ -0,0 +1,14 @@
+package com.tiempo.learn;
+
+public class Demo02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int lives = 0;
+		if (lives < 1) 
+			System.out.println("Game Over!");
+		else
+			System.out.println("Are you are still alive");
+	}
+
+}
